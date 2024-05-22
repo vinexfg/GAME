4a25cd531c6240a2f251801b37bb6ef76b679625
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-05-22 20:44:44 -0300

    a

--- udemy.py
+++ udemy.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 
+import numpy as np
 
 
 
-plt.plot(vendas_meses)
+
+vendas = np.random.randint(1000, 3000, 50)
+meses =np.arange(1, 51)
+
+print(vendas)
+print(meses)
