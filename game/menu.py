ffd4a0a140b2ce2222c64639c7d157f2edd38549
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-30 21:57:05 -0300

    Update menu.py

---
+++ game/menu.py
@@ -0,0 +1,20 @@
+print('Bem vindo ao meu Game')
+
+
+def mostrar_menu():
+    print('===*4=== MENU ===*4===')
+    print('1. Novo Jogo')
+    print('2. Carregar jogo')
+    print('3. configuração')
+    print('4. Sair')
+    print('=========================')
+
+    def escolher_opcao():
+        opcao = input('Escolher uma opcao')
+        return opcao
+    
+    def novo_jogo():
+        print('Iniciando o jogo...')
+    
+    def carregar_jogo():
+        print('Carregando jogo salvo')
