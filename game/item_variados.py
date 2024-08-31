34b016c9da41c2f84486b1db9df7e06659df3fc7
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-30 21:56:52 -0300

    a

---
+++ game/item_variados.py
@@ -0,0 +1,19 @@
+
+
+class Itens:
+    def __init__(self):
+        self.itens_por_elemento = {
+            'fogo': [],
+            'agua': [],
+            'terra': [],
+            'ar': []
+        }
+        
+    def adicionar_item(self, elemento, item):
+        if elemento in self.itens_por_elemento:
+            self.itens_por_elemento[elemento].append(item)
+        else:
+            print('Elemento invalido!')
+
+
+        
